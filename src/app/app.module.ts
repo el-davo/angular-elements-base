5a537fc56db95e73a757589503d0d59d691ea885
el-davo <david.ahern@emc.com>
2020-07-16 14:06:24 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {Injector, NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {HelloWorldComponent} from './hello-world/hello-world.component';
+import {createCustomElement} from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HelloWorldComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [
+    HelloWorldComponent
+  ],
+  providers: []
 })
-export class AppModule { }
+export class AppModule {
+  constructor(injector: Injector) {
+    const custom = createCustomElement(HelloWorldComponent, {injector});
+
+    customElements.define('app-hello-world', custom);
+  }
+
+  ngDoBootstrap() {
+  }
+}
